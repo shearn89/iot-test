3281203bd3e65406b8d34dfa75f1a24012e52d71
shearn89 <alexander.shearn@gmail.com>
2016-10-31 21:35:59 +0000

    Working upload to initialstate

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# AIO-EnviroPi #
+# IOT-EnviroPi #
 
-Simple python script to send data to http://io.adafruit.com
+Simple python script to send data to https://app.initialstate.com
 
 Collects all data from the Enviro pHAT and sends it up to the cloud.
+
+## Installation ##
+
+* Copy `enviropi.service` to `/etc/systemd/system/`
+* `systemctl enable enviropi.service`
+* `systemctl start enviropi.service`
--- enviro.py
+++ enviro.py
@@ -1,22 +1,31 @@
 # Based heavily on https://blog.bartbania.com/raspberry_pi/temperature-log-howto/
 
+import time
 import sys
+import logging
 
 from subprocess import PIPE, Popen
 from envirophat import light, weather, leds
-from Adafruit_IO import *
+
+from ISStreamer.Streamer import Streamer
 
 FACTOR = 1.3
+
+logging.basicConfig(level=logging.INFO)
 
 with open('keyfile.txt', 'r') as f:
     api_key = f.read().strip()
 
-aio = Client(api_key)
+streamer = Streamer(bucket_name="Pi Zero", bucket_key="shearn89-pizero", access_key=api_key)
 
 def get_environment():
-    data = {'pi-zero-temp-board': weather.temperature(), 'pi-zero-pressure': weather.pressure(), 'pi-zero-light': light.light()}
-    data['pi-zero-temp-cpu'] = get_cpu_temperature()
-    data['pi-zero-temp-actual'] = data['pi-zero-temp-board'] - ((data['pi-zero-temp-cpu'] - data['pi-zero-temp-board'])/FACTOR)
+    data = {
+        'board_temp': round(weather.temperature(),2),
+        'pressure': round(weather.pressure(),0),
+        'light': round(light.light(),2)
+    }
+    data['cpu_temp'] = get_cpu_temperature()
+    data['est_temp'] = data['board_temp'] - ((data['cpu_temp'] - data['board_temp'])/FACTOR)
 
     return data
 
@@ -24,15 +33,17 @@ def get_cpu_temperature():
     process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
     output, _error = process.communicate()
 
-    return float(output[output.index('=') + 1:output.rindex("'")])
+    return round(float(output[output.index('=') + 1:output.rindex("'")]),2)
 
 def send_data(data):
+    logging.debug('sending data')
     for feed,value in data.iteritems():
-        aio.send(feed, value)
+        logging.debug("%s,%.2f" % (feed,value))
+        streamer.log(feed,value)
 
 if __name__ == '__main__':
-    data = get_environment()
-    send_data(data)
-
-    sys.exit()
-
+    logging.info("Starting temp logger...")
+    while True:
+        data = get_environment()
+        send_data(data)
+        time.sleep(60)
